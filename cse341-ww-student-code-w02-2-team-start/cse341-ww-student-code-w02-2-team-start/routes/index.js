87e4468802c3b279e5039b128b7cbb632350d77b
John Bright Agbons <brightinojohnny@gmail.com>
2025-01-20 21:35:23 +0100

    commit

--- cse341-ww-student-code-w02-2-team-start/cse341-ww-student-code-w02-2-team-start/routes/index.js
+++ cse341-ww-student-code-w02-2-team-start/cse341-ww-student-code-w02-2-team-start/routes/index.js
@@ -1,5 +1,8 @@
 const routes = require('express').Router();
 const temple = require('./temple');
+const { ObjectId } = require('mongodb');
+const db = require('../db'); // Assuming `db` is your database connection module
+
 
 routes.use('/temples', temple);
 routes.use(
@@ -13,84 +16,70 @@ routes.use(
 );
 
 
-/**
- * @swagger
- * /temples/{id}:
- *   get:
- *     summary: Get a single temple by ID
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         description: The ID of the temple
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Details of the temple
- *       404:
- *         description: Temple not found
- */
-router.get('/temples/:id', (req, res) => {
-  res.send('Get temple by ID');
+
+// GET all temples
+router.get('/temples', async (req, res) => {
+  try {
+    const temples = await db.collection('temples').find().toArray();
+    res.status(200).json(temples);
+  } catch (err) {
+    res.status(500).json({ message: 'Error retrieving temples', error: err });
+  }
 });
 
-/**
-* @swagger
-* /temples/{id}:
-*   put:
-*     summary: Update a temple's information
-*     parameters:
-*       - name: id
-*         in: path
-*         required: true
-*         description: The ID of the temple to update
-*         schema:
-*           type: string
-*     requestBody:
-*       description: The updated temple data
-*       required: true
-*       content:
-*         application/json:
-*           schema:
-*             type: object
-*             properties:
-*               name:
-*                 type: string
-*               location:
-*                 type: string
-*               description:
-*                 type: string
-*     responses:
-*       200:
-*         description: Temple successfully updated
-*       400:
-*         description: Invalid input
-*/
-router.put('/temples/:id', (req, res) => {
-  res.send('Update temple by ID');
+// GET a temple by ID
+router.get('/temples/:id', async (req, res) => {
+  try {
+    const temple = await db.collection('temples').findOne({ _id: new ObjectId(req.params.id) });
+    if (temple) {
+      res.status(200).json(temple);
+    } else {
+      res.status(404).json({ message: 'Temple not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Error retrieving temple', error: err });
+  }
 });
 
-/**
-* @swagger
-* /temples/{id}:
-*   delete:
-*     summary: Delete a temple
-*     parameters:
-*       - name: id
-*         in: path
-*         required: true
-*         description: The ID of the temple to delete
-*         schema:
-*           type: string
-*     responses:
-*       200:
-*         description: Temple successfully deleted
-*       404:
-*         description: Temple not found
-*/
-router.delete('/temples/:id', (req, res) => {
-  res.send('Delete temple by ID');
+// POST a new temple
+router.post('/temples', async (req, res) => {
+  try {
+    const result = await db.collection('temples').insertOne(req.body);
+    res.status(201).json({ message: 'Temple created', id: result.insertedId });
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating temple', error: err });
+  }
 });
 
-module.exports = routes;
+// PUT to update a temple by ID
+router.put('/temples/:id', async (req, res) => {
+  try {
+    const result = await db.collection('temples').updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+    if (result.modifiedCount > 0) {
+      res.status(200).json({ message: 'Temple updated' });
+    } else {
+      res.status(404).json({ message: 'Temple not found or no changes made' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating temple', error: err });
+  }
+});
+
+// DELETE a temple by ID
+router.delete('/temples/:id', async (req, res) => {
+  try {
+    const result = await db.collection('temples').deleteOne({ _id: new ObjectId(req.params.id) });
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: 'Temple deleted' });
+    } else {
+      res.status(404).json({ message: 'Temple not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting temple', error: err });
+  }
+});
+
+module.exports = router;
--- cse341-ww-student-code-w02-2-team-start/cse341-ww-student-code-w02-2-team-start/routes/temple.js
+++ cse341-ww-student-code-w02-2-team-start/cse341-ww-student-code-w02-2-team-start/routes/temple.js
@@ -1,26 +1,12 @@
-const routes = require('express').Router();
-const temples = require('../controllers/temple.js');
+const express = require('express');
+const router = express.Router();
 
-routes.get('/', temples.findAll);
-routes.get('/:temple_id', temples.findOne);
+const usersController = require('../controllers/users');
 
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
+router.post('/', usersController.createUser);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
-// Update a temple's information
-routes.put('/temples/:id', (req, res) => {
-    const templeId = req.params.id;
-    const updatedTemple = req.body;
-    // Logic to update temple information in the database
-    res.send(`Temple with ID: ${templeId} has been updated`);
-});
-
-// Delete a temple
-routes.delete('/temples/:id', (req, res) => {
-    const templeId = req.params.id;
-    // Logic to delete a temple from the database
-    res.send(`Temple with ID: ${templeId} has been deleted`);
-});
-
-
-routes.post('/', temples.create);
-
-module.exports = routes;
+module.exports = router;
