6223ff5e077625f9574b8c140fc149a4f263fa0f
John Bright Agbons <brightinojohnny@gmail.com>
2025-01-20 23:19:03 +0100

    commit

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,6 +3,7 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAllContacts);
 router.get('/:id', contactsController.getContactById);
+router.post('/', contactsController.createUser); // Corrected POST route
 
 module.exports = router;
 
