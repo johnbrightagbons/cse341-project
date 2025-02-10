8c87768c45bede97d7710460f360693a802fcb86
John Bright Agbons <brightinojohnny@gmail.com>
2025-02-10 15:56:29 +0100

    commit

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/schoolAPI';
+const uri = process.env.MONGODB_URI;
 const dbName = 'schoolAPI';
 let db;
 
